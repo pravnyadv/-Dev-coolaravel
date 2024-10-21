37d5da79d0e6d8c70403e0f3dadf40d0ded112f6
Praveen yadav <py3008@gmail.com>
2024-10-21 16:43:54 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,20 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    build: {
+        lib: {
+            entry: 'resources/js/app.js',  // Specify your entry point
+            name: 'MyLibrary',
+            fileName: (format) => `my-library.${format}.js`
+        },
+        rollupOptions: {
+            // Externalize dependencies you don’t want bundled into the library
+            external: ['vue'],
+            output: {
+                globals: {
+                    vue: 'Vue'
+                }
+            }
+        }
+    }
 });
